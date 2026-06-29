d079b672e73047a5855964b265763a6529796b60
fourarmsrk-max <fourarmsrk@gmail.com>
2026-06-29 12:36:32 +0530

    Update middlewares.py

--- bot/middlewares.py
+++ bot/middlewares.py
@@ -1,1 +1,16 @@
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Callable, Dict, Any, Awaitable
+from database.base import async_session
 
+class DbSessionMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any]
+    ) -> Any:
+        async with async_session() as session:
+            data["db"] = session
+            return await handler(event, data)
